c2f956bcdaca293652d013238e8487c4ca3ac6f8
Joshua Arpon <joshua.arpon@wsp.com>
2023-08-15 14:00:04 -0700

    Added python files

---
+++ BranchCodeSample/Code1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 15 13:57:48 2023
+
+@author: joshua.arpon
+"""
+
+print('sample')
---
+++ BranchCodeSample/Code2.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 15 13:59:05 2023
+
+@author: joshua.arpon
+"""
+a = 'sample 2'
+print(a)
